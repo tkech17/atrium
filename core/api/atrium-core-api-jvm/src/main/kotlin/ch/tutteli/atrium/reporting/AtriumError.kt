28c902a9983c94ac2fd2a6a2e6d388045eb91c72
Robert Stoll <rstoll@tutteli.ch>
2020-02-18 22:41:22 +0100

    print error message only once for IntelliJ runners

--- core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/reporting/AtriumError.kt
+++ core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/reporting/AtriumError.kt
@@ -13,6 +13,16 @@ import ch.tutteli.atrium.reporting.AtriumError.Companion
  */
 actual class AtriumError internal actual constructor(message: String) : AssertionError(message, null) {
 
+    // Spek as well as the JUnit Jupiter runner in IntelliJ use message instead of localizedMessage
+    // (which is kind of a bug so this might change at any time). With this hack we can prevent that the error message
+    // is printed twice in the console
+    override val message: String = ""
+
+    override fun getLocalizedMessage(): String? = super.message
+
+    //print first the message and then the qualifiedName + stack (Throwable prints qualifiedName and then message)
+    override fun toString(): String = localizedMessage + "\n\n" + this::class.qualifiedName
+
     actual companion object {
         /**
          * * Creates an [AtriumError] and adjusts it with the given [atriumErrorAdjuster] before it is returned
